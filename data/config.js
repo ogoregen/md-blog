3054e37ae44e3408ebdcd5d27618cd2bfc457d67
ogoregen <37447279+ogoregen@users.noreply.github.com>
2024-10-25 19:00:49 +0300

    Update default config

--- data/config.js
+++ data/config.js
@@ -1,7 +1,7 @@
 
 const CONFIG = {
     title: "md-blog",
-    description: "nothing less nor more than a blog",
+    description: "nothing less or more than a blog",
     url: "https://ogoregen.github.io/md-blog/",
     footerText: "CC-BY-SA",
 };
