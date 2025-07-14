657a562450eee759e3c4d74759e5db8474af4704
ogoregen <ogoregen@deico.com.tr>
2025-07-14 08:30:27 +0300

    Style: Remove trailing whitespace

--- src/components/metadata.jsx
+++ src/components/metadata.jsx
@@ -6,7 +6,7 @@ const MetaData = ({title, description, children}) => {
 	return(
         <>
             <title>{title}</title>
-            <meta name="description" content={description}/>                        
+            <meta name="description" content={description}/>
             {children}
         </>
 	);
