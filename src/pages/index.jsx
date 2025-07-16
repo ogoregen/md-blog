89c2434f779ca424b64feb0b3e2baf8ed952a93a
ogoregen <ogoregen@deico.com.tr>
2025-07-16 15:28:20 +0300

    Fix post date style

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -43,11 +43,11 @@ const Blog = ({data}) => {
                 data.allMarkdownRemark.nodes.map(node => (
                     <div className="fly-margin-large-bottom" key={node.id}>
                         <div className="fly-flex fly-flex-space-between fly-margin-bottom fly-flex-break-mobile">
-                            <div>
+                            <div className="fly-margin-right">
                                 <Link to={`/${node.frontmatter.slug}`} className="fly-text-lead">{node.frontmatter.title}</Link>
                                 <p className="fly-margin-none">{node.frontmatter.description}</p>
                             </div>
-                            <p className="fly-margin-none fly-text-muted">{node.frontmatter.date}</p>
+                            <p className="fly-margin-none fly-text-muted" style={{flexShrink: 0}}>{node.frontmatter.date}</p>
                         </div>
                     </div>
                 ))
