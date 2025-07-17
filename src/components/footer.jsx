734dbad06f02affd30ef388938ea562c810b6e5d
ogoregen <ogoregen@deico.com.tr>
2025-07-17 08:54:04 +0300

    Refactor style.css

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,7 +5,7 @@ import CONFIG from "../../data/config";
 const Footer = () => {
 
 	return(
-		<footer className="fly-footer fly-text-center fly-margin-bottom">
+		<footer className="fly-text-center fly-margin-bottom">
 			<span>{CONFIG.footerText}</span>
 			<a href="/rss.xml" target="_blank" className="fly-margin-left">RSS</a>
 		</footer>
